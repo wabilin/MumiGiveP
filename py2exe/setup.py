37649d161e687a036e1c928349961ecd6aaef98f
Potato <twelendil@gmail.com>
2017-03-03 11:26:23 +0800

    add py2exe scripts

---
+++ py2exe/setup.py
@@ -0,0 +1,10 @@
+﻿from distutils.core import setup
+import py2exe
+
+setup(
+    options = {'py2exe': {
+        'bundle_files': 1
+    }},
+    console = [{'script': 'mumi.py'}],
+    zipfile = None
+)

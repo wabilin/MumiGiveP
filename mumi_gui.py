0faeba91475d3803d5c73d149e828bfb84b5a684
Potato <twelendil@gmail.com>
2017-03-06 17:16:04 +0800

    GUI works

--- mumi_gui.py
+++ mumi_gui.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
+import tkMessageBox
+from ptt_agent import auto_give_money
+from ptt_html import push_list_from_url
+from push_filter import filter_push_list
 
 
 class MumiGui:
@@ -22,18 +26,9 @@ class MumiGui:
 
         self._create_duplicate_option(5)
 
-        self.var = BooleanVar(value=True)
-        c = Checkbutton(root, text=u"不重複發送給同ㄧID", variable=self.var,
-                        onvalue=True, offvalue=False)
-        c.grid(row=5, column=0)
-
         self._create_give_push_options(6)
 
-        self.console = Text(root, width=80, height=10, bg='black', fg='white')
-        self.console.configure(state='disabled')
-        self.console.grid(row=8, columnspan=4)
-
-        self.go_button = Button(root, text=u"發射姆咪", command=lambda x: x)
+        self.go_button = Button(root, text=u"發射姆咪", command=self.go_mu_mi)
         self.go_button.grid(row=7, column=3)
 
     def _create_url_field(self, row):
@@ -49,7 +44,7 @@ class MumiGui:
     def _create_account_field(self, row):
         root = self.root
 
-        id_entry = Entry(root)
+        id_entry = Entry(root, show='*')
         password_entry = Entry(root, show='*')
 
         id_entry.grid(row=row, column=1, sticky=W)
@@ -78,17 +73,17 @@ class MumiGui:
         money_en.grid(row=row, column=3, sticky=W)
 
         Label(root, text=u"樓發一次").grid(row=row+1, column=1, sticky=W)
-        floor_en = Entry(root, width=2)
-        floor_en.insert(END, '1')
-        floor_en.grid(row=row+1, column=0, sticky=E)
+        step_en = Entry(root, width=2)
+        step_en.insert(END, '1')
+        step_en.grid(row=row+1, column=0, sticky=E)
 
         self.entries['amount'] = amount_en
         self.entries['money'] = money_en
-        self.entries['floor'] = floor_en
+        self.entries['step'] = step_en
 
     def _create_duplicate_option(self, row):
-        self.entries['duplicate'] = BooleanVar(value=True)
-        c = Checkbutton(self.root, text=u"不重複發送給同ㄧID",
+        self.entries['duplicate'] = BooleanVar(value=False)
+        c = Checkbutton(self.root, text=u"可重複發送給同ㄧ位鄉民",
                         variable=self.entries['duplicate'],
                         onvalue=True, offvalue=False)
         c.grid(row=row, column=0)
@@ -107,11 +102,51 @@ class MumiGui:
                                 onvalue=True, offvalue=False)
             check.grid(row=start_row, column=i)
 
+    def go_mu_mi(self):
+        allowed_push = [x for x in range(1, 4) if self.give_push[x].get()]
+        step = to_int(self.entries['step'].get())
+        duplicate = bool(self.entries['duplicate'].get())
+        amount = to_int(self.entries['amount'].get())
+
+        opt = {
+            'allowed_types': allowed_push,
+            'floor_limit': None,
+            'step': step,
+            'duplicate': duplicate,
+            'amount': amount
+        }
+
+        push_list = push_list_from_url(self.entries['url'].get())
+
+        lst = filter_push_list(push_list, opt)
+
+        for name in lst:
+            self.show(name)
+
+        r = tkMessageBox.askquestion(u"要發錢囉", u"真的嗎?", icon='warning')
+        if r == 'yes':
+            user = {'id': self.entries['id'].get(),
+                    'password': self.entries['password'].get()}
+
+            self.root.after(100,
+                            lambda: auto_give_money(to_int(self.entries['money'].get()),
+                                                    lst, user,
+                                                    printer=self.show))
+
     def show(self, msg):
-        self.console.insert('end', msg + "\n")
+        print msg
 
     def run(self):
         self.root.mainloop()
+
+
+def to_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        tkMessageBox.showinfo("Error",
+                              "Con not convert {} to integer.".format(s))
+        return None
 
 
 def main():

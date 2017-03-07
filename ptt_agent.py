975bd5dfc995c07848059b0a15dada6efef0b0ce
Potato <twelendil@gmail.com>
2017-03-07 16:41:44 +0800

    not bad

--- ptt_agent.py
+++ ptt_agent.py
@@ -13,9 +13,12 @@ def ptt_to_utf8(ptt_msg):
     return ptt_msg.decode('uao_decode').encode('utf8')
 
 
-class PttAction:
-    def __init__(self, expected, keyin, opt_acts=None, newline=True):
-        pass
+class PttAct:
+    def __init__(self, expected, key_in, opt_acts=None, newline=True):
+        self.expected = expected
+        self.key_in = key_in
+        self.opt_acts = opt_acts
+        self.newline = newline
 
 
 class PttIo:
@@ -29,10 +32,10 @@ class PttIo:
         self.printer = callbacks['printer']
         self.failed = callbacks['failed']
 
-    def clear_buffer(self):
+    def _clear_buffer(self):
         self.buffer = ''
 
-    def expect_action(self, expected, res, opt_acts=None, newline=True):
+    def _expect_action(self, expected, res, opt_acts=None, newline=True):
         msg, buf, telnet = '', self.buffer, self.tn
 
         waiting_time = 0.2
@@ -43,61 +46,75 @@ class PttIo:
             msg = ptt_to_utf8(buf)
 
             if expected in msg:
-                self.log(msg)
+                self._write_log(msg)
                 enter_msg(self.tn, res, newline)
 
-                self.clear_buffer()
-                return
+                self._clear_buffer()
+                return True
 
             elif opt_acts:
                 matched = [x for x in opt_acts if x[0] in msg]
                 if matched:
-                    self.log(msg)
+                    self._write_log(msg)
                     enter_msg(self.tn, matched[0][1], newline)
                     buf = ''
 
-        self.log(msg)
-        self.timeout_fail()
+        self._write_log(msg)
+        return False
+
+    def _expect_actions(self, actions):
+        for act in actions:
+            r = self._expect_action(act.expected, act.key_in,
+                                    act.opt_acts, act.newline)
+            if not r:
+                return False
+        return True
+
+    def _write_log(self, msg):
+        print >>self._log, msg
 
     def login(self):
-        self.expect_action("註冊", self.account)
-        self.expect_action("請輸入您的密碼", self.password)
+        actions = [PttAct("註冊", self.account),
+                   PttAct("請輸入您的密碼", self.password)]
+        return self._expect_actions(actions)
 
     def go_store(self):
-        self.expect_action("主功能表", 'p',
-                           opt_acts=[["請按任意鍵繼續", ''],
-                                     ["刪除其他", 'y'],
-                                     ["錯誤嘗試的記錄", 'n']])
-
-        self.expect_action("網路遊樂場", 'p')
+        actions = [PttAct("主功能表", 'p',
+                          opt_acts=[["請按任意鍵繼續", ''],
+                                    ["刪除其他", 'y'],
+                                    ["錯誤嘗試的記錄", 'n']]),
+                   PttAct("網路遊樂場", 'p')]
+        return self._expect_actions(actions)
 
     def give_money(self, name, money):
         if name.lower() == self.account.lower():
             self.printer("Can not give money to yourself.")
-            return
+            return True
 
-        # Assert in ptt store page
-        self.expect_action("給其他人Ptt幣", '0')
-        self.expect_action("這位幸運兒的id", name)
-        self.expect_action("請輸入金額", money)
-        self.expect_action("要修改紅包袋嗎", 'n',
-                           opt_acts=[["請輸入您的密碼", self.password],
-                                     ["確定進行交易嗎", 'y']])
-
-        self.expect_action("按任意鍵繼續", '')
-
-    def log(self, msg):
-        print >>self._log, msg
+        # Assert start from ptt store page
+        actions = [PttAct("給其他人Ptt幣", '0'),
+                   PttAct("這位幸運兒的id", name),
+                   PttAct("請輸入金額", money),
+                   PttAct("要修改紅包袋嗎", 'n',
+                          opt_acts=[["請輸入您的密碼", self.password],
+                                    ["確定進行交易嗎", 'y']]),
+                   PttAct("按任意鍵繼續", '')]
+        return self._expect_actions(actions)
 
     def logout(self):
         self.tn.write(b'\x1b[D')
-
-        self.expect_action("按任意鍵繼續", '',
-                           opt_acts=[["網路遊樂場", b'\x1b[D'],
+        self._expect_action("按任意鍵繼續", '',
+                            opt_acts=[["網路遊樂場", b'\x1b[D'],
                                      ["主功能表", "G\r\n"],
                                      ["您確定要離開", "y\r\n"]],
-                           newline=False)
+                            newline=False)
+        return True
 
+    @property
+    def log(self):
+        return self._log
+
+    '''
     def timeout_fail(self):
         print "Can not get response in time."
         log_file = open('log.txt', 'w')
@@ -105,6 +122,7 @@ class PttIo:
         self._log.close()
         log_file.close()
         self.failed()
+    '''
 
 
 def enter_msg(tn, msg, newline):

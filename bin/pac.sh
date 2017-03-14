5409633c933ea60b492e53d3db9b535c81a69c17
Potato <twelendil@gmail.com>
2017-03-14 14:29:45 +0800

    pac.sh: a little simplify

--- bin/pac.sh
+++ bin/pac.sh
@@ -2,48 +2,38 @@
 
 ver="$1"
 os="$2"
+name_head="MumiGiveP_"
+    mumi_path="../mumi/mumi.py"
+
+function make_bin_zip {
+    options="$1"
+    tail="$2"
+
+    n_name="$name_head$ver"
+    z_name="$n_name""_""$os$tail"
+
+    pyinstaller $options $mumi_path
+    cp ../README.md ./dist
+    cp ../THANKS.md ./dist
+
+    rm -r build
+    mv ./dist "./$n_name"
+
+    if [ "$os" == "win" ]; then
+        7z a -tzip $z_name $n_name
+    else
+        zip -r -X $z_name $n_name
+    fi
+
+    rm -r $n_name
+}
 
 if [ "$#" -lt "2" ]; then
     echo "need a least 2 args"
     echo "Get: $# ."
+
 else
-    name_head="MumiGiveP_"
-    mumi_path="../mumi/mumi.py"
-
-    n_name="$name_head$ver"
-    tail_1=".zip"
-    tail_2="_console.zip"
-    z_name="$n_name""_""$os$tail_1"
-
-    # Normal version
-    pyinstaller --onefile --noconsole $mumi_path
-    cp ../README.md ./dist
-    rm -r build
-    mv ./dist "./$n_name"
-
-    if [ "$os" == "win" ]; then
-        7z a -tzip $z_name $n_name
-    else
-        zip -r -X $z_name $n_name
-    fi
-
-    rm -r $n_name
-
-
-    # Console version
-    z_name="$n_name""_""$os$tail_2"
-
-    pyinstaller $mumi_path
-    cp ../README.md ./dist
-    rm -r build
-    mv ./dist "./$n_name"
-
-    if [ "$os" == "win" ]; then
-        7z a -tzip $z_name $n_name
-    else
-        zip -r -X $z_name $n_name
-    fi
-
-    rm -r $n_name
+    make_bin_zip "--onefile --noconsole" ".zip"
+    make_bin_zip "" "_console.zip"
 fi
 

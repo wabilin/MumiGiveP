ead6ed9757ae4b34b8ece1c186af9236621c437f
wabilin <twelendil@gmail.com>
2018-03-13 21:12:10 +0800

    挖坑之前最好多想三秒鐘

--- mumi.js
+++ mumi.js
@@ -1,72 +1,73 @@
-let getPttChrome = () => {
+const getPttChrome = () => {
   return window.wrappedJSObject.pttchrome.app;
 };
 
-let arrowLeft = () => {
+const arrowLeft = () => {
   ptt.conn.send('\x1b[D');
 };
 
 // setInterval(arrowLeft, 3000);
 
-let getBbsLines = () => {
+const getBbsLines = () => {
   return document.querySelectorAll('[data-type="bbsline"]');
 };
 
-let all_span = $("span:not(:has(*))");
-all_span.each(function(i) {
-  let x = $(this);
-  t = 'mumi'+ x.text();
-});
-
-let pushTypes = {
+const pushTypes = {
   PUSH: 1,
   ARROW: 2,
   BOO: 3
 };
 
-let parsePushData = (line) => {
-  if (!isPushLine(line)) {
+const pushType = (span) => {
+  const matchClassAndHtml = (ele, cName, html) => {
+    return ele.className === cName && ele.innerHTML === html;
+  };
+
+  const isPush =  (span) => matchClassAndHtml(span, "q15 b0", "<span>推 </span>");
+  const isArrow = (span) => matchClassAndHtml(span, "q9 b0", "<span>→ </span>");
+  const isBoo =   (span) => matchClassAndHtml(span, "q9 b0", "<span>噓 </span>");
+  const typeDetectors = { 1: isPush, 2: isArrow, 3: isBoo };
+
+  for (let key of Object.keys(typeDetectors)) {
+    const detector = typeDetectors[key];
+    if (detector(span)) {
+      return key;
+    }
+  }
+
+  return null;
+};
+
+const pushLineFormat = (line) => {
+  return ('children' in line) && ('length' in line.children) &&
+    (line.children.length === 4);
+};
+
+const matchIdRule = (id) => {
+  if (!id) {
+    return false;
+  }
+  const re = /^[A-z][A-z0-9]{3,}$/;
+  return re.test(id);
+};
+
+const parsePushData = (line) => {
+  if (!pushLineFormat(line)) {
     return null;
   }
 
-  let head = line.children[0];
-  let id = line.children[1].children[0].innerHTML;
-  
-  let pushType = pushTypes.ARROW;
-  if (isPush(head)) {
-    pushType = pushTypes.PUSH;
-  } else if (isBoo(head)) {
-    pushType = pushTypes.ARROW;
+  const head_span = line.children[0];
+  const type = pushType(head_span);
+
+  const idSpan = line.children[1];
+  const id = idSpan && idSpan.children[0] && idSpan.children[0].innerHTML;
+
+  if (!type || !matchIdRule(id)) {
+    return null;
   }
 
   return {
     pushType: pushType,
     id: id
-  }
+  };
 };
-
-let matchClassAndHtml = (ele, cName, html) => {
-  return ele.className === cName && ele.innerHTML === html;
-};
-
-let isPush =  (span) => matchClassAndHtml(span, "q15 b0", "<span>推 </span>"); 
-let isArrow = (span) => matchClassAndHtml(span, "q9 b0", "<span>→ </span>"); 
-let isBoo =   (span) => matchClassAndHtml(span, "q9 b0", "<span>噓 </span>");
-
-let matchIdRule = (id) => {
-  let re = /^[A-z][A-z0-9]{3,}$/;
-  return re.test(id);
-};
-
-let isPushLine = (line) => {
-  if(! 'children' in line || ! 'length' in line.children ||
-    line.children.length !== 4) {
-    return false;
-  }
-  
-  let head = line.children[0];
-  let id = line.children[1].children[0].innerHTML;
-
-  return (isPush(head) || isArrow(head) || isBoo(head)) && matchIdRule(id);
-};
-
